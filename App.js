65caef7d98ee1261cb22019a1d0df1dfe3ddc9fb
translatorzepp <3465940+translatorzepp@users.noreply.github.com>
2019-09-13 14:55:39 -0500

    Delete unused style

--- App.js
+++ App.js
@@ -142,11 +142,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
   prompt: {
     textAlign: 'center',
     color: '#333333',
