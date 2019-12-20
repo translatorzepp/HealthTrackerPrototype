85672dfa705e3bb7841ace5041ea2ae04e0c418b
translatorzepp <3465940+translatorzepp@users.noreply.github.com>
2019-12-20 17:08:48 -0600

    wip tests

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -20,6 +20,19 @@ it('renders correctly', () => {
   expect(tree).toMatchSnapshot();
 });
 
+it('requests stored items for the day', () => {
+  const app = renderer.create(<App />);
+
+  expect(AsyncStorage.getItem).toHaveBeenNthCalledWith(1, '20190913')
+  expect(AsyncStorage.getItem).toHaveBeenNthCalledWith(2, '20190913')
+});
+
+// it('displays stored items for the day', () => {
+//   const tree = renderer.create(<App />).toJSON();
+//   // TODO: mock retrieved data
+//   expect(tree).toMatchSnapshot();
+// });
+
 afterAll(() => {
   MockDate.reset();
 });
